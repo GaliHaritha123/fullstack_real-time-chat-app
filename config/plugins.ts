ca73a4b317329c2c5a0763e3eb7e5946d3a74f0f
GaliHaritha123 <harithagali79@gmail.com>
2025-02-15 14:43:38 +0530

    Added jwtSecret for authentication

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,8 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  // Other plugins...
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET', '3g+Kb5+MP+i894SvxGKC1Q==')
+    },
+  },
+});
