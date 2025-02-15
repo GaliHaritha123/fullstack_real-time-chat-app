d52ff7765fc6c46b7ca9958534ec809867592d41
GaliHaritha123 <harithagali79@gmail.com>
2025-02-15 14:51:22 +0530

    Added apiToken.salt for Strapi admin security

--- config/admin.ts
+++ config/admin.ts
@@ -1,17 +1,9 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'your-secret-key-here'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
-  },
-  transfer: {
-    token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
-    },
-  },
-  flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    salt: env('API_TOKEN_SALT', 'VSGY6OIYZLCvOi8/qIW4wA==') // Use your generated salt
   },
 });
+
